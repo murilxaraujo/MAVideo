14fb206071280a07e107d9bee6863168b17c87a0
Murilo Araujo <muernandes@icloud.com>
2020-09-26 23:03:01 -0300

    Initial Commit

---
+++ MAVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAVideo
+//
+//  Created by Murilo Araujo on 26/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
